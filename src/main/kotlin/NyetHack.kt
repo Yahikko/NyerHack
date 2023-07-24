a9f7d3810a9328c2296f370216b46179737578ed
Yahikko <taczuhiro@mail.ru>
2023-07-24 11:37:09 +0300

    lists; iterations; read from file

--- src/main/kotlin/NyetHack.kt
+++ src/main/kotlin/NyetHack.kt
@@ -1,13 +1,12 @@
+var heroName: String = ""
+
 fun main() {
-    narrate(
-        message = "A hero enters the town of Kronstadt. What is their name?"
-    ) { message -> "\u001b[36;1m$message\u001b[0m" }
-    val heroName = readlnOrNull()
-    require(!heroName.isNullOrEmpty()) {
-        "The hero must have a name."
-    }
-    changeNarratorMood()
+
+    heroName = promptHeroName()
+//    changeNarratorMood()
     narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
+
+    visitTavern()
 }
 
 fun createTitle(name: String): String {
@@ -17,4 +16,17 @@ fun createTitle(name: String): String {
         name.none { it.isLetter() } -> "The Witness Protection Member"
         else -> "The Renowned Hero"
     }
+}
+
+private fun promptHeroName(): String {
+    narrate("A hero enters the town of Kronstadt. What is their name?") { message ->
+        "\u001b[36;1m$message\u001b[0m"
+    }
+    /*val input = readlnOrNull()
+    require(!heroName.isNullOrEmpty()) {
+        "The hero must have a name."
+    }
+    return input*/
+    println("Madrigal")
+    return "Madrigal"
 }
---
+++ src/main/kotlin/Tavern.kt
@@ -0,0 +1,62 @@
+import java.io.File
+
+private const val TAVERN_MASTER = "Taernyl"
+private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
+
+private val menuData = File("data/tavern-menu-data.txt")
+    .readText()
+    .split("\n")
+
+private val menuItems = List(menuData.size) { index ->
+    val (_, name, _) = menuData[index].split(",")
+    name
+}
+
+fun visitTavern() {
+    narrate("$heroName enters $TAVERN_NAME")
+    narrate("There are several items for sale:")
+    println(menuItems)
+
+    val patrons = mutableListOf("Eli", "Mordoc", "Sophie")
+
+    val eliMessage = if (patrons.contains("Eli")) {
+        "$TAVERN_MASTER says: Eli's in the back playing cards"
+    } else {
+        "$TAVERN_MASTER says: Eli isn't here"
+    }
+    println(eliMessage)
+
+    val otherMessage = if (patrons.containsAll(listOf("Sophie", "Mordoc"))) {
+        "$TAVERN_MASTER says: Sophie and Mordoc are seated by the stew kettle"
+    } else {
+        "$TAVERN_MASTER says: Sophie and Mordoc aren't with each other right now"
+    }
+    println(otherMessage)
+
+//    narrate("Eli leaves the tavern")
+//    patrons.remove("Eli")
+//    narrate("Alex enters the tavern")
+//    patrons.add("Alex")
+//    println(patrons)
+//    narrate("Alex (VIP) enters the tavern")
+//    patrons.add(0, "Alex")
+//    println(patrons)
+//    patrons[0] = "Alexis"
+//    println(patrons)
+
+//    for (patron in patrons) {
+//        println("Good evening, $patron")
+//    }
+
+//    patrons.forEach { patron -> println("Good evening, $patron") }
+
+    patrons.forEachIndexed { index, patron ->
+        println("Good evening, $patron - you're #${index + 1} in line")
+        placeOrder(patron, menuItems.random())
+    }
+}
+
+private fun placeOrder(patronName: String, menuItemName: String) {
+    narrate("$patronName speaks with $TAVERN_MASTER to place an order")
+    narrate("$TAVERN_MASTER hands $patronName a $menuItemName")
+}
