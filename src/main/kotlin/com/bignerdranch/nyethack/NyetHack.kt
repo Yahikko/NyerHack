ae98591ea08336e2517a197f9618b53189ba7313
Yahikko <taczuhiro@mail.ru>
2023-07-28 15:57:30 +0300

    Objects; data classes; ADT (Algebraic Data Types): enum, sealed; value classes

--- src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -4,25 +4,11 @@ lateinit var player: Player
 
 fun main() {
 
-//    changeNarratorMood()
-
     narrate("Welcome to NyetHack!")
+//    changeNarratorMood()
     val playerName = promptHeroName()
     player = Player(playerName)
-
-    player.prophesize()
-
-    val mortality = if (player.isImmortal) "an immortal" else "a mortal"
-    val currentRoom: Room = Tavern()
-
-    narrate("${player.name} of ${player.hometown}, ${player.title} is in ${currentRoom.description()}")
-    narrate("${player.name}, $mortality, has ${player.healthPoints} health points")
-    currentRoom.enterRoom()
-
-    player.prophesize()
-    player.castFireball()
-
-    //visitTavern()
+    Game.play()
 }
 
 private fun promptHeroName(): String {
@@ -36,4 +22,67 @@ private fun promptHeroName(): String {
     return input*/
     println("Madrigal")
     return "Madrigal"
+}
+
+object Game {
+
+    private val worldMap = listOf(
+        listOf(TownSquare(), Tavern(), Room("Back Room")),
+        listOf(Room("A Long Corridor"), Room("A Generic Room")),
+        listOf(Room("The Dungeon"))
+    )
+
+    private var currentPosition = Coordinate(0, 0)
+    private var currentRoom: Room = worldMap[0][0]
+
+    init {
+        narrate("Welcome, adventurer")
+        val mortality = if (player.isImmortal) "an immortal" else "a mortal"
+        narrate("${player.name}, $mortality, has ${player.healthPoints} health points")
+    }
+
+    fun play() {
+        while (true) {
+            narrate("${player.name} of ${player.hometown}, ${player.title} is in ${currentRoom.description()}")
+            currentRoom.enterRoom()
+
+            print("> Enter your command: ")
+            GameInput(readln()).processCommand()
+        }
+    }
+
+    fun move(direction: Direction) {
+        val newPosition = direction.updateCoordinate(currentPosition)
+        val newRoom = worldMap.getOrNull(newPosition.y)?.getOrNull(newPosition.x)
+
+        if (newRoom != null) {
+            narrate("The hero moves ${direction.name}")
+            currentPosition = newPosition
+            currentRoom = newRoom
+        } else {
+            narrate("You cannot move ${direction.name}")
+        }
+    }
+
+    private class GameInput(arg: String?) {
+
+        private val input = arg ?: ""
+        val command = input.split(" ")[0]
+        val argument = input.split(" ").getOrElse(1) { "" }
+
+        fun processCommand() = when (command.lowercase()) {
+            "move" -> {
+                val direction = Direction.entries.firstOrNull() {
+                    it.name.equals(argument, ignoreCase = true)
+                }
+                if (direction != null) {
+                    move(direction)
+                } else {
+                    narrate("I don't know what direction that is")
+                }
+            }
+
+            else -> narrate("I'm not sure what you're trying to do")
+        }
+    }
 }
